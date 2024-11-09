10276c96c6a1dec2f8ed925dbd3bb8709fbcafcf
minji2219 <200146@jnu.ac.kr>
2024-11-09 17:02:57 +0900

    feat: 재고 관리 클래스 추가
    입력한 상품이 존재하는 상품인지 확인하는 함수
    프로모션, 일반 상품을 합한 양을 초과하지 않는지 확인하는 함수

---
+++ src/InventoryManagement.js
@@ -0,0 +1,49 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class InventoryManagement {
+  constructor(inventory, purchase) {
+    this.inventory = inventory;
+    this.purchaseItems = purchase;
+    this.purchaseIndexList = [];
+
+    this.checkCanPurchase();
+  }
+
+  checkCanPurchase() {
+    try {
+      this.purchaseItems.forEach((item) => {
+        this.checkExistence(item);
+        Console.print(this.purchaseIndexList);
+      });
+    } catch (err) {
+      Console.print(err);
+      //다시 입력 받기
+    }
+  }
+
+  //존재하는 상품을 입력했는지
+  checkExistence(item) {
+    const index = this.inventory.findIndex(
+      (product) => product.name === item.name
+    );
+    if (index === -1)
+      throw new Error('[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.');
+    this.purchaseIndexList.push(index);
+    this.checkAmount(index, item.amount);
+  }
+
+  //재고 수량을 초과하지 않는지
+  checkAmount(index, amount) {
+    let totalAmount = this.inventory[index].amount;
+    if (this.inventory[index].promotion !== 'null')
+      totalAmount += this.inventory[index + 1].amount;
+
+    if (totalAmount < amount) {
+      throw new Error(
+        '[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.'
+      );
+    }
+  }
+}
+
+export default InventoryManagement;
