215df16b20dfbda932c713b68373a239bf13ff26
minji2219 <200146@jnu.ac.kr>
2024-11-09 14:28:17 +0900

    feat: 구매할 상품, 가격 객체 데이터 생성
    입력 받은 문자열을 쉼표로 구분하고, 앞 뒤 괄호를 제거
    {상품이름, 구매수량} 데이터 생성

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,6 +1,7 @@
 import { Console } from '@woowacourse/mission-utils';
 
 class InputView {
+  purchaseList;
   constructor() {
     this.readItem();
   }
@@ -13,16 +14,30 @@ class InputView {
   }
 
   validationForm(input) {
+    this.purchaseList = [];
     const pattern = /^\[[\p{L}]+-\d+\](,\[[\p{L}]+-\d+\])*$/u;
     try {
       if (!input.match(pattern))
         throw new Error(
           '[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.'
         );
+      this.makePurchaseArr(input);
     } catch (err) {
       Console.print(err);
       this.readItem();
     }
+  }
+
+  makePurchaseArr(input) {
+    input.split(',').map((purchase) => {
+      const removeParentheses = purchase
+        .slice(1, purchase.length - 1)
+        .split('-');
+      this.purchaseList.push({
+        name: removeParentheses[0],
+        amount: removeParentheses[1],
+      });
+    });
   }
 }
 export default InputView;
