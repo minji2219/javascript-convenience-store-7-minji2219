5f4102fc89fa5cfc4a744a13b7222a189a851f59
minji2219 <200146@jnu.ac.kr>
2024-11-11 00:55:07 +0900

    feat: 총출력 구현

--- src/View/Receipt.js
+++ src/View/Receipt.js
@@ -13,26 +13,29 @@ class Receipt {
     this.promotionBuy = promotionBuy;
     this.presentation = presentation;
     this.discount = discount;
+    this.totalAmount = 0;
+    this.totalPrice = 0;
     this.printReceipt();
   }
 
   printReceipt() {
-    //18 = ~ 의, 10 점 ~ =
     Console.print('==============W 편의점================');
     Console.print('상품명'.padEnd(18, ' ') + '수량'.padEnd(10, ' ') + '금액');
     this.printPurchase();
-    Console.print('=============증	정===============');
+    Console.print('=============증    정===============');
     this.printPresentation();
     Console.print('====================================');
+    this.printTotal();
   }
 
   printPurchase() {
     this.itemIndex.map((item, index) => {
       const productName = this.inventory[item].name;
       const amount = this.promotionBuy[index] + this.commonBuy[index];
-      const price = this.inventory[item].price;
-
-      Console.print(productName.padEnd(18, ' ') + amount.toString().padEnd(10, ' ') + price);
+      const price = this.inventory[item].price * amount;
+      this.totalAmount += amount;
+      this.totalPrice += price;
+      Console.print(productName.padEnd(18, ' ') + amount.toString().padEnd(10, ' ') + price.toLocaleString());
     });
   }
 
@@ -46,6 +49,12 @@ class Receipt {
     });
   }
 
-  printTotal() {}
+  printTotal() {
+    const payPrice = this.totalPrice - this.discount.event - this.discount.membership;
+    Console.print('총구매액'.padEnd(18, ' ') + this.totalAmount.toString().padEnd(10, ' ') + this.totalPrice.toLocaleString());
+    Console.print('행사할인'.padEnd(28, ' ') + '-' + this.discount.event.toLocaleString());
+    Console.print('멤버십할인'.padEnd(28, ' ') + '-' + this.discount.membership.toLocaleString());
+    Console.print('내실돈'.padEnd(28, ' ') + payPrice.toLocaleString());
+  }
 }
 export default Receipt;
