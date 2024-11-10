09ddd322947bbb15bd8696c3b2f9ddcbbf60e56d
minji2219 <200146@jnu.ac.kr>
2024-11-11 00:53:23 +0900

    comment: 주석 및 불필요한 console 삭제

--- src/Discount.js
+++ src/Discount.js
@@ -1,10 +1,3 @@
-// 멤버십 할인
-// 멤버십 회원은 프로모션 미적용 금액의 30%를 할인받는다.
-// 프로모션 적용 후 남은 금액에 대해 멤버십 할인을 적용한다.
-// 멤버십 할인의 최대 한도는 8,000원이다.
-
-import {Console} from '@woowacourse/mission-utils';
-
 class Discount {
   constructor(promotionBuy, commonBuy, inventory, purchaseIndex, presentation) {
     this.membershipDiscounts = 0;
@@ -20,13 +13,10 @@ class Discount {
   }
 
   eventDiscount() {
-    Console.print(this.presentation);
     this.promotionBuy.map((buy, index) => {
-      Console.print(this.presentation);
       const price = this.inventory[this.purchaseIndex[index]].price;
       this.eventDiscounts += price * this.presentation[index];
     });
-    Console.print('이벤트 할인' + this.eventDiscounts);
     this.eventDiscounts;
   }
 
@@ -35,7 +25,6 @@ class Discount {
       this.membershipDiscounts += this.inventory[this.purchaseIndex[index]].price * Number(buy);
     });
     this.membershipDiscounts *= 0.3;
-    Console.print('멤버십' + this.membershipDiscounts);
     if (this.membershipDiscounts > 8000) return 8000;
     return this.membershipDiscounts;
   }
