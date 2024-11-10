9e928186b83c8e0d5951e556c80b5e5ce09e405d
minji2219 <200146@jnu.ac.kr>
2024-11-10 15:55:56 +0900

    feat: 프로모션 데이터 get, buy 타입 number로 변경

--- src/ReadPromotion.js
+++ src/ReadPromotion.js
@@ -1,22 +1,18 @@
-import { Console, DateTimes } from '@woowacourse/mission-utils';
-import { readFileSync } from 'node:fs';
+import {Console, DateTimes} from '@woowacourse/mission-utils';
+import {readFileSync} from 'node:fs';
 
 class ReadPromotion {
-  constructor(purchaseIndex) {
+  constructor() {
     this.promotions = [];
     this.readPromotion();
   }
 
   readPromotion() {
-    const promotionString = readFileSync('./public/promotions.md', 'utf8')
-      .toString()
-      .trim()
-      .split('\n');
+    const promotionString = readFileSync('./public/promotions.md', 'utf8').toString().trim().split('\n');
     promotionString.shift();
     promotionString.map((promotion) => {
       const promotionInfo = promotion.split(',');
-      if (this.isPromotionPeriod(promotionInfo[3], promotionInfo[4]))
-        this.promotions.push(this.makePromotion(promotionInfo));
+      if (this.isPromotionPeriod(promotionInfo[3], promotionInfo[4])) this.promotions.push(this.makePromotion(promotionInfo));
     });
   }
 
@@ -31,13 +27,9 @@ class ReadPromotion {
   makePromotion(promotion) {
     return {
       name: promotion[0],
-      buy: promotion[1],
-      get: promotion[2],
+      buy: Number(promotion[1]),
+      get: Number(promotion[2]),
     };
   }
-  //1. 프로모션 상품인 경우, 수량이 남아있다면
-  //프로모션 기간 여부를 확인하고 프로모션 적용이 되는지 확인한다.
-  //적용이 된다면,
-  //2. 수량이 없다면
 }
 export default ReadPromotion;
